611d2879760d5b9e8cc6b173c786e49b693f1b7e
saminfatehir <saminfatehir@gmail.com>
2016-05-17 18:49:02 +0430

    make vector_1

--- graph.cpp
+++ graph.cpp
@@ -42,6 +42,8 @@ public:
 class Node
 {
 	friend class Naber;
+//	friend void operator=(Naber&, Naber&);
+
 private:
 	char name;
 	bool type;
@@ -65,6 +67,21 @@ public:
 		this -> type = type;
 		this -> color = color;
 	}
+	
+	char get_name()
+	{
+		return name;
+	}
+	
+	bool get_type()
+	{
+		return type;
+	}
+	
+	string get_color()
+	{
+		return color;
+	}
 
 	void firstPrint ()
 	{
@@ -75,38 +92,53 @@ public:
 
 class Naber
 {
+// friend void operator=(Naber&, Naber&);
 private:
 	Node *node;
 	int weight;
 
 public:
-	Naber(Node *node, int weight)
+	Naber(Node node, int weight)
 	{
-		this -> node = new Node(node->name, node->type, node->color);
+		this -> node = new Node(node.name, node.type, node.color);
 		this -> weight = weight;
+	}
+	Naber (){}
+	
+	void set_node(Node node)
+	{
+		this->node->setFirstNode(node.get_name(), node.get_type(), node.get_color());
 	}
 
 
 };
 
+void operator=( Naber& N1,Naber& N2)
+{
+	N1.node->name = N2.node->name;
+}
+
 int main()
 {
 	char name;
-	char naber;
+	char neighbor;
 	bool type;
 	string color;
 	int weight;
 	int ptr;
+	int vecCounter = 0, vecCounter2 = 0;
 
 	Triangle tri(0, 0);
 	Circle cir(0, 0, 0);
 
 	const int size = 6;
 	Node nodeArr[size];
+//	nodeArr2<vector<Node *>,size>;
 
-	vector <vector <Node *>> node;
-	Node *n;
-	n = new Node(' ', false, 0);
+	vector <vector <Naber *> > neighborArr;
+	vector <Naber *> neighbors; 
+//	Node *n;
+//	n = new Node(' ', false, 0);
 	vector <Node *> node2;
 	vector <Node *> node3;
 	vector <Node *> node4;
@@ -123,6 +155,8 @@ int main()
 
 	for (int i = 0; graphFile >> name >> type >> color; i++)
 	{
+		Node nod(name, type, color) ;
+		nodeArr[i] = nod;
 		//node.push_back(n);
 		//node[i]->setFirstNode(name,  type, color);
 		//node[i]->firstPrint();
@@ -130,25 +164,44 @@ int main()
 			break;
 	}
 
-	for (int i = 0; graphFile >> naber >> weight; i++)
+	for (int i = 0; graphFile >> neighbor >> weight; i++)
 	{
 		//node.push_back(n);
 		//node[i]->setSecondNode(naber, weight);
 		//node[i]->secondPrint();
-		graphFile >> naber;
-		if (naber != '/')
+//		graphFile >> neighbor;
+		for(int j = 0; j < size; j++)
+		{
+			if(neighbor == nodeArr[j].get_name())
+			{
+				Node node1('a',1,"green");
+				Naber n;
+				n.set_node(node1);
+				 
+				Naber nbr(nodeArr[j] , (int)weight);
+				neighborArr.push_back(neighbors); 
+				neighborArr[vecCounter][vecCounter2]->set_node(node1);
+				vecCounter2++;
+				break;
+			}
+		}
+		if (neighbor != '/')
 		{
 			i--;
+			vecCounter2 = 0;
+			vecCounter++;
 			ptr = graphFile.tellg();
 			graphFile.seekg(ptr-1);
 		}
 
-		else
+/*		else
 		{
 			cout << "\n";
 		}
-	}
+	*/	
 
-	system ("PAUSE");
+	}
+		for(int i = 0; i < size; i++)
+			nodeArr[i].firstPrint();
 	return 0;
 }
